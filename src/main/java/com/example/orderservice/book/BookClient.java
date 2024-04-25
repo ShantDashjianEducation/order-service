8c8326f5055a6a186022c232c7ce94060991c81d
Shant Dashjian <shantgdashjian@gmail.com>
2024-04-25 16:52:34 +0400

    Add 3 resilience mechanisms from Reactor

--- src/main/java/com/example/orderservice/book/BookClient.java
+++ src/main/java/com/example/orderservice/book/BookClient.java
@@ -1,8 +1,12 @@
 package com.example.orderservice.book;
 
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -17,6 +21,12 @@ public class BookClient {
 			.get()
 			.uri(BOOKS_ROOT_API + isbn)
 			.retrieve()
-			.bodyToMono(Book.class);
+			.bodyToMono(Book.class)
+			.timeout(Duration.ofSeconds(3), Mono.empty())
+			.onErrorResume(WebClientResponseException.NotFound.class,
+				exception -> Mono.empty())
+			.retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+			.onErrorResume(Exception.class,
+				exception -> Mono.empty());
 	}
 }
